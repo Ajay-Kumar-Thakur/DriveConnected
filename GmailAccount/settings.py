a98dd088dc562aadae5e6f5903b5b3e5b2fd8e09
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-05-04 17:06:16 +0545

    add vercel config

--- GmailAccount/settings.py
+++ GmailAccount/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # settings.py
 import os
+from dotenv import load_dotenv
+load_dotenv()
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'GmailAccount.urls'
@@ -121,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
